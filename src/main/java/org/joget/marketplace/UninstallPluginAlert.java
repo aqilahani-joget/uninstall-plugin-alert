4ee3ebf238a669c631231c1a532a8ab75447e6d8
aqilahani-joget <nor.aqilahani@joget.com>
2025-11-03 11:58:02 +0800

    comment out unused methods

--- src/main/java/org/joget/marketplace/UninstallPluginAlert.java
+++ src/main/java/org/joget/marketplace/UninstallPluginAlert.java
@@ -183,119 +183,120 @@ public class UninstallPluginAlert extends UiHtmlInjectorPluginAbstract implement
         return apps;
     }
 
-    public Set<Map<String, String>> executeQery(String pluginClass, Set<Map<String, String>> apps) {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
+    // public Set<Map<String, String>> executeQery(String pluginClass, Set<Map<String, String>> apps) {
+    //     Connection con = null;
+    //     PreparedStatement pstmt = null;
+    //     ResultSet rs = null;
         
-        String sql = getSql();
-        try {
-            DataSource ds = (DataSource)AppUtil.getApplicationContext().getBean("setupDataSource");
-            con = ds.getConnection();
+    //     String sql = getSql();
+    //     try {
+    //         DataSource ds = (DataSource)AppUtil.getApplicationContext().getBean("setupDataSource");
+    //         con = ds.getConnection();
 
-            PreparedStatement ps = con.prepareStatement(sql);
+    //         PreparedStatement ps = con.prepareStatement(sql);
 
-            String searchPattern = "%" + pluginClass + "%"; // your search string
-            int i = 1;
-            ps.setString(i++, searchPattern); // builder.json LIKE ?
-            ps.setString(i++, searchPattern); // form.json LIKE ?
-            ps.setString(i++, searchPattern); // datalist.json LIKE ?
-            ps.setString(i++, searchPattern); // userview.json LIKE ?
-            ps.setString(i++, pluginClass);      // plugin_default.id = ?
-            ps.setString(i++, searchPattern); // rsc.permissionProperties LIKE ?
-            ps.setString(i++, searchPattern); // pkg_act_plugin.pluginProperties LIKE ?
-            ps.setString(i++, searchPattern); // pkg_participant.pluginProperties LIKE ?
+    //         String searchPattern = "%" + pluginClass + "%"; // your search string
+    //         int i = 1;
+    //         ps.setString(i++, searchPattern); // builder.json LIKE ?
+    //         ps.setString(i++, searchPattern); // form.json LIKE ?
+    //         ps.setString(i++, searchPattern); // datalist.json LIKE ?
+    //         ps.setString(i++, searchPattern); // userview.json LIKE ?
+    //         ps.setString(i++, pluginClass);      // plugin_default.id = ?
+    //         ps.setString(i++, searchPattern); // rsc.permissionProperties LIKE ?
+    //         ps.setString(i++, searchPattern); // pkg_act_plugin.pluginProperties LIKE ?
+    //         ps.setString(i++, searchPattern); // pkg_participant.pluginProperties LIKE ?
 
-            rs = ps.executeQuery();
+    //         rs = ps.executeQuery();
 
-            while (rs.next()) {
-                Map<String, String> appIdName = new HashMap<>();
-                // Get columns by name
-                String appId = rs.getString("appId");
-                String appName = rs.getString("name");
+    //         while (rs.next()) {
+    //             Map<String, String> appIdName = new HashMap<>();
+    //             // Get columns by name
+    //             String appId = rs.getString("appId");
+    //             String appName = rs.getString("name");
 
-                appIdName.put(appId, appName);
-                apps.add(appIdName);
-            }
+    //             appIdName.put(appId, appName);
+    //             apps.add(appIdName);
+    //         }
 
-        } catch (SQLException | BeansException e) {
-            LogUtil.error(getClassName(), e, "");
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-                if (con != null) {
-                    con.close();
-                }
-            } catch (SQLException e) {
-                LogUtil.error(getClassName(), e, "");
-            }
-        }
+    //     } catch (SQLException | BeansException e) {
+    //         LogUtil.error(getClassName(), e, "");
+    //     } finally {
+    //         try {
+    //             if (rs != null) {
+    //                 rs.close();
+    //             }
+    //             if (pstmt != null) {
+    //                 pstmt.close();
+    //             }
+    //             if (con != null) {
+    //                 con.close();
+    //             }
+    //         } catch (SQLException e) {
+    //             LogUtil.error(getClassName(), e, "");
+    //         }
+    //     }
         
-        return apps;
-    }
+    //     return apps;
+    // }
 
     private String getPublishedAppsSql() {
         return "SELECT app.appId, app.name, app.appVersion " +
                 "FROM app_app app " +
                 "WHERE app.published = 1 ";
     }
-    private String getSql() {
-        return "SELECT app.appId, app.name " +
-                "FROM app_app app " +
-                "WHERE app.published = 1 " +
-                "AND EXISTS ( " +
-                "    SELECT 1 FROM ( " +
-                "        SELECT appId FROM app_builder " +
-                "            WHERE app_builder.appId = app.appId " +
-                "              AND app_builder.appVersion = app.appVersion " +
-                "              AND app_builder.json LIKE ? " +
-                "        UNION ALL " +
-                "        SELECT appId FROM app_form " +
-                "            WHERE app_form.appId = app.appId " +
-                "              AND app_form.appVersion = app.appVersion " +
-                "              AND app_form.json LIKE ? " +
-                "        UNION ALL " +
-                "        SELECT appId FROM app_datalist " +
-                "            WHERE app_datalist.appId = app.appId " +
-                "              AND app_datalist.appVersion = app.appVersion " +
-                "              AND app_datalist.json LIKE ? " +
-                "        UNION ALL " +
-                "        SELECT appId FROM app_userview " +
-                "            WHERE app_userview.appId = app.appId " +
-                "              AND app_userview.appVersion = app.appVersion " +
-                "              AND app_userview.json LIKE ? " +
-                "        UNION ALL " +
-                "        SELECT appId FROM app_plugin_default " +
-                "            WHERE app_plugin_default.appId = app.appId " +
-                "              AND app_plugin_default.appVersion = app.appVersion " +
-                "              AND app_plugin_default.id = ? " +
-                "        UNION ALL " +
-                "        SELECT appId FROM app_resource " +
-                "            WHERE app_resource.appId = app.appId " +
-                "              AND app_resource.appVersion = app.appVersion " +
-                "              AND app_resource.permissionProperties LIKE ? " +
-                "        UNION ALL " +
-                "        SELECT p.appId FROM app_package_activity_plugin " +
-                "            INNER JOIN app_package p ON p.packageId = app_package_activity_plugin.packageId " +
-                "              AND p.packageVersion = app_package_activity_plugin.packageVersion " +
-                "            WHERE p.appId = app.appId " +
-                "              AND p.appVersion = app.appVersion " +
-                "              AND app_package_activity_plugin.pluginProperties LIKE ? " +
-                "        UNION ALL " +
-                "        SELECT appId FROM app_package_participant " +
-                "            INNER JOIN app_package p ON p.packageId = app_package_participant.packageId " +
-                "              AND p.packageVersion = app_package_participant.packageVersion " +
-                "            WHERE p.appId = app.appId " +
-                "              AND p.appVersion = app.appVersion " +
-                "              AND app_package_participant.pluginProperties LIKE ? " +
-                "    ) AS tmp " +  
-                ")";
-    }
+    
+    // private String getSql() {
+    //     return "SELECT app.appId, app.name " +
+    //             "FROM app_app app " +
+    //             "WHERE app.published = 1 " +
+    //             "AND EXISTS ( " +
+    //             "    SELECT 1 FROM ( " +
+    //             "        SELECT appId FROM app_builder " +
+    //             "            WHERE app_builder.appId = app.appId " +
+    //             "              AND app_builder.appVersion = app.appVersion " +
+    //             "              AND app_builder.json LIKE ? " +
+    //             "        UNION ALL " +
+    //             "        SELECT appId FROM app_form " +
+    //             "            WHERE app_form.appId = app.appId " +
+    //             "              AND app_form.appVersion = app.appVersion " +
+    //             "              AND app_form.json LIKE ? " +
+    //             "        UNION ALL " +
+    //             "        SELECT appId FROM app_datalist " +
+    //             "            WHERE app_datalist.appId = app.appId " +
+    //             "              AND app_datalist.appVersion = app.appVersion " +
+    //             "              AND app_datalist.json LIKE ? " +
+    //             "        UNION ALL " +
+    //             "        SELECT appId FROM app_userview " +
+    //             "            WHERE app_userview.appId = app.appId " +
+    //             "              AND app_userview.appVersion = app.appVersion " +
+    //             "              AND app_userview.json LIKE ? " +
+    //             "        UNION ALL " +
+    //             "        SELECT appId FROM app_plugin_default " +
+    //             "            WHERE app_plugin_default.appId = app.appId " +
+    //             "              AND app_plugin_default.appVersion = app.appVersion " +
+    //             "              AND app_plugin_default.id = ? " +
+    //             "        UNION ALL " +
+    //             "        SELECT appId FROM app_resource " +
+    //             "            WHERE app_resource.appId = app.appId " +
+    //             "              AND app_resource.appVersion = app.appVersion " +
+    //             "              AND app_resource.permissionProperties LIKE ? " +
+    //             "        UNION ALL " +
+    //             "        SELECT p.appId FROM app_package_activity_plugin " +
+    //             "            INNER JOIN app_package p ON p.packageId = app_package_activity_plugin.packageId " +
+    //             "              AND p.packageVersion = app_package_activity_plugin.packageVersion " +
+    //             "            WHERE p.appId = app.appId " +
+    //             "              AND p.appVersion = app.appVersion " +
+    //             "              AND app_package_activity_plugin.pluginProperties LIKE ? " +
+    //             "        UNION ALL " +
+    //             "        SELECT appId FROM app_package_participant " +
+    //             "            INNER JOIN app_package p ON p.packageId = app_package_participant.packageId " +
+    //             "              AND p.packageVersion = app_package_participant.packageVersion " +
+    //             "            WHERE p.appId = app.appId " +
+    //             "              AND p.appVersion = app.appVersion " +
+    //             "              AND app_package_participant.pluginProperties LIKE ? " +
+    //             "    ) AS tmp " +  
+    //             ")";
+    // }
 
     private static String normalizeJarName(String filename) {
         if (filename == null) return "";
